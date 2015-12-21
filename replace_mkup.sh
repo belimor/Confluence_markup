5bdb6b3e0b09177f0606aa3d1cec314f6347137b
Dmitry <dmitry.vaghin@cybera.ca>
2015-12-21 22:59:59 +0000

    some code added

--- replace_mkup.sh
+++ replace_mkup.sh
@@ -1,81 +1,175 @@
 #!/bin/bash
+mydebug="y"
 
 if [ -z $1 ] 
 then
   echo "Enter path to the file: replace_mkup.sh ../techdocs/mainSpace/"
   exit 1
 else
-  for i in $( ls $1 ); do
-    echo "File -> $i"
+  cd $1
+ # for i in * ; do
+  find . -iname "*" | while read i
+  do
+    if [ "$i" != "." ]; then
+    echo " " >> "${i}"
+    echo "File: $i"
       while read line
   	do
       	name=$line
 
+	if [[ $name == *"</span>"* ]]; then
+	  name="$(echo "$name" | sed -e 's/<[^>]*>//g')"
+	  #name=$(echo "$name" | sed 's/<\/span>//g')
+          #name=$(echo "$name" | sed "s/<span*>//g")
+	  if [ "$mydebug" == "y" ]; then
+              echo "o:=> $line"
+              echo "n:=> $name"
+              echo ""
+          fi
+	fi
+
 	if [[ $name == *"//"* ]]; then
-	  name=$(echo $name | sed 's/\/\//_/g')
-          echo "o:=> $line"
-          echo "n:=> $name"
-          echo ""
+          if [[ $name == *"http//"* ]] || [[ $name == *"https//"* ]]; then
+            name=$(echo $name | sed 's/\/\//_/g')
+            if [ "$mydebug" == "y" ]; then
+              echo "o:=> $line"
+              echo "n:=> $name"
+              echo "/////////////"
+            fi
+            echo "$name" >> "${i}.new"
+          fi
+        fi
+ 
+	if [[ $name == *"**"* ]]; then
+	echo "**"
+	needle1="**"
+        number_of_occurrences1=$(grep -o "$needle1" <<< "$name" | wc -l)
+	echo "$number_of_occurrences1"
+	if [ $number_of_occurrences1 -gt 1 ]; then
+	  name=$(echo "$name" | sed 's/\*\*/\*/g')
+	  if [ "$mydebug" == "y" ]; then
+              echo "o:=> $line"
+              echo "n:=> $name"
+              echo ""
+          fi
+	fi
+	fi
+ 
+	needle="\[\["
+	number_of_occurrences=$(grep -o "$needle" <<< "$name" | wc -l)
+	if [ "$mydebug" == "y" ]; then
+        echo "ns:=> $number_of_occurrences"
 	fi
 
 	if [ "$(echo -n $name | tail -c 3)" = "===" ]; then
            name=$(echo $name | sed 's/^===/h3. /')
            name=$(echo $name | sed 's/===/ /')
-	   echo "o:=> $line"
-	   echo "n:=> $name"
-	   echo ""
+	   if [ "$mydebug" == "y" ]; then
+	     echo "o:=> $line"
+	     echo "n:=> $name"
+	     echo ""
+	   fi
+	   echo "$name" >> "${i}.new"
 	else 
 	  if [ "$(echo -n $name | tail -c 2)" = "==" ]; then
-	   name=$(echo $name | sed 's/^==/h2. /')
-           name=$(echo $name | sed 's/==/ /')
-	   echo "o:=> $line"
-           echo "n:=> $name" 
-	   echo ""
+	    name=$(echo $name | sed 's/^==/h2. /')
+            name=$(echo $name | sed 's/==/ /')
+	    if [ "$mydebug" == "y" ]; then
+	      echo "o:=> $line"
+              echo "n:=> $name" 
+	      echo ""
+	    fi
+	    echo "$name" >> "${i}.new"
 	  else 
 	    if [ "$(echo -n $name | tail -c 1)" = "=" ]; then
               name=$(echo $name | sed 's/^=/h1. /')
               name=$(echo $name | sed 's/=/ /')
-	      echo "o:=> $line"
-              echo "n:=> $name" 
-	      echo ""
+	      if [ "$mydebug" == "y" ]; then
+	        echo "o:=> $line"
+                echo "n:=> $name" 
+	        echo ""
+	      fi
+	      echo "$name" >> "${i}.new"
   	    else
-		if [[ $name == *"[[@"* ]]; then
-		  if [[ $(echo -n "$name" | grep -o -P '(?<=\[\[).*(?=\]\])') == *"|"* ]]; then
+	      if [[ $name == *"[[@"* ]] && [ $number_of_occurrences -lt 2 ]; then
+	        if [[ $(echo -n "$name" | grep -o -P '(?<=\[\[).*(?=\]\])') == *"|"* ]]; then
 		  name=$(echo -n "$name" | sed "s/\[\[\@/\[\[/")
-		  part1=$(echo -n "$name" | grep -o -P '(?<=\[\[).*(?=\|)')
-		  part2=$(echo -n "$name" | grep -o -P '(?<=\|).*(?=\]\])')
-		  echo "o:=> ${line}"
-		  part0=$(echo -n "$name" | awk -F[ {'print $1'})
-                  part3=$(echo -n "$name" | awk -F] {'print $2'})
+		  part1="$(echo -n "$name" | grep -o -P '(?<=\[\[).*(?=\|)')"
+		  part2="$(echo -n "$name" | grep -o -P '(?<=\|).*(?=\]\])')"
+		  part0="$(echo -n "$name" | awk -F[ {'print $1'})"
+                  part3="$(echo -n "$name" | awk -F] {'print $2'})"
 		  name="${part0}[$part2|$part1]$part3"
-		  echo "n:=> $name"
-		  echo ""
+		  if [ "$mydebug" == "y" ]; then
+		    echo "o:=> ${line}"
+		    echo "n:=> $name"
+		    echo ""
 		  fi
-		else
-		  if [[ $name == *"[["* ]]; then
-		    if [[ $(echo -n "$name" | grep -o -P '(?<=\[\[).*(?=\]\])') == *"|"* ]]; then
-                      part1=$(echo -n "$name" | grep -o -P '(?<=\[\[).*(?=\|)')
-                      part2=$(echo -n "$name" | grep -o -P '(?<=\|).*(?=\]\])')
-                      echo "o:=> ${line}"
-                      part0=$(echo -n "$name" | awk -F[ {'print $1'})
-                      part3=$(echo -n "$name" | awk -F] {'print $2'})
-		      name="${part0}[$part2|$part1]$part3"
+		  echo "$name" >> "${i}.new"
+		fi
+	      else
+	        if [[ $name == *"[["* ]] && [ $number_of_occurrences -lt 2 ]; then
+		  if [[ $(echo -n "$name" | grep -o -P '(?<=\[\[).*(?=\]\])') == *"|"* ]]; then
+                    part1=$(echo -n "$name" | grep -o -P '(?<=\[\[).*(?=\|)')
+                    part2=$(echo -n "$name" | grep -o -P '(?<=\|).*(?=\]\])')
+                    part0=$(echo -n "$name" | awk -F[ {'print $1'})
+                    part3=$(echo -n "$name" | awk -F] {'print $2'})
+		    name="${part0}[$part2|$part1]$part3"
+		    if [ "$mydebug" == "y" ]; then
+		      echo "o:=> ${line}"
                       echo "n:=> $name"
                       echo ""
-                    else
-		      name=$(echo -n "$name" | sed 's/\[\[/\[/')
-		      name=$(echo -n "$name" | sed 's/\]\]/\]/')
+		    fi
+   		    echo "$name" >> "${i}.new"
+                  else
+		    name=$(echo -n "$name" | sed 's/\[\[/\[/')
+		    name=$(echo -n "$name" | sed 's/\]\]/\]/')
+			if [[ $name == *"[code]"* ]]; then
+                        name=$(echo $name | sed 's/\[code\]/\{code:\}/g')
+                        fi
+		    if [ "$mydebug" == "y" ]; then
                       echo "o:=> ${line}"
                       echo "n:=> ${name}"
                       echo ""
-                    fi
-                  fi 
+		    fi
+		    echo "$name" >> "${i}.new"
+                  fi
+		else
+		  if [[ $name == *"[["* ]]; then
+		    name=$(echo -n "$name" | sed 's/\[\[/\[/')
+		    name=$(echo -n "$name" | sed 's/\]\]/\]/')
+ 			if [[ $name == *"[code]"* ]]; then
+        		name=$(echo $name | sed 's/\[code\]/\{code:\}/g')
+        		fi
+		    if [ "$mydebug" == "y" ]; then
+		      echo "o:=> ${line}"
+                      echo "n:=> ${name}"
+                      echo ""
+		    fi
+		    echo "$name" >> "${i}.new"
+		  else
+		        if [[ $name == *"[code]"* ]]; then
+        		name=$(echo $name | sed 's/\[code\]/\{code:\}/g')
+        		fi
+		    if [ "$mydebug" == "y" ]; then
+		      echo "o:=> ${line}"
+		      echo "n:=> ${name}"
+                      echo ""
+		    fi
+		    echo "$name" >> "${i}.new"
+		  fi
 		fi
+	      fi
             fi
 	  fi	
         fi
-	
 
-      done < ${1}${i}
+	if [ "$mydebug" == "y" ]; then
+	echo "=========================================="
+	fi
+	j=$i
+      done < "${i}"
+    fi
   done
 fi
+echo $name
+echo $i
